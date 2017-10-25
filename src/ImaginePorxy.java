88a518af872dc50eb8cffde5bf185d679021d2b3
vasy <vasy@vasy-PC>
2017-10-25 12:35:02 +0300

    lab4.b

---
+++ src/ImaginePorxy.java
@@ -0,0 +1,37 @@
+
+public class ImaginePorxy extends AbstractElement {
+	private String numeImagine;
+	private Imagine realImage= null;
+	public  ImaginePorxy (String numeImagine){
+		this.numeImagine=numeImagine;
+	}
+	
+
+	@Override
+	public void addElement(Element elem) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Element elem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Element getChild(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+}
