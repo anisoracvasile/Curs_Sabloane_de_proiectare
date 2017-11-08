6c4570e36d8213fe5821f74306fe5c4861b50a1e
vasy <vasy@10.3.1.237>
2017-11-08 13:51:56 +0200

    completare lab6

--- src/DocumentStatisticVisitor.java
+++ src/DocumentStatisticVisitor.java
@@ -9,31 +9,57 @@ public class DocumentStatisticVisitor implements Visitor {
 	
 	public void visitimagineProxy(ImaginePorxy img) {
 	System.out.println(img);
+	 numarImagini++;
+	 
 		
 	}
 
 
 	public void visitImagine(Imagine img) {
 		System.out.println(img);
+		 numarImagini++;
+		
 		
 	}
 
 
 	public void visitParagraf(Paragraf parag) {
 		System.out.println(parag);
+		numarParagrafe++;
 		
 	}
 
 
 	public void visitTabel(Tabel tabl) {
 		System.out.println(tabl);
+		numarTabele++;
 		
 	}
 
 
 	public void visitSectiune(Sectiune sect) {
 		System.out.println(sect);
-		
+		 numarSectiuni++;
 	}
+	
+	  public int getNumarTabele()
+	    {
+	        return numarTabele;
+	    }
+	    
+	    public int getNumarParagrafe()
+	    {
+	        return numarParagrafe;
+	    }
+	    
+	    public int getNumarImagini()
+	    {
+	        return numarImagini;
+	    }
+	    
+	    public int getNumarSectiuni()
+	    {
+	        return numarSectiuni;
+	    }
 
 }
